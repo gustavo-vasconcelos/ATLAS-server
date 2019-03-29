fef23aedc480a8d770c906bfa13221cfd7a2c404
Carlos Pinto Guedes <carlosguedespinto@gmail.com>
2019-03-29 12:50:31 +0000

    Events schema

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,2 +1,39 @@
 const mongoose = require("mongoose")
 
+const UserSchema = new mongoose.Schema({
+    id: Number,
+    profileId: Number,
+    name: String,
+    username: {
+        type: String,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        select: false
+    },
+    email: String,
+    picture: String,
+    gender: Number,
+    accountCreation: {
+        date: String,
+        hour: String
+    },
+    interests: {
+        tags: [Number],
+        courses: [Number],
+        proponents: [Number]
+    },
+    notifications: [
+        {
+            id: Number,
+            eventId: Number,
+            matching: {
+                tags: [Number],
+                courses: [Number],
+                proponents: [Number]
+            },
+            moment: String
+        }
+    ]
+})
