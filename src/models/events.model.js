3909630c958cf96dd912b4b5c4fd210bcb504f4c
Carlos Pinto Guedes <carlosguedespinto@gmail.com>
2019-03-29 12:37:25 +0000

    Events schema

---
+++ src/models/events.model.js
@@ -0,0 +1,54 @@
+const mongoose = required("mongoose")
+
+const EventSchema = new mongoose.Schema({
+    authorId: Number,
+    name: String,
+    category: String,
+    tags: [Number],
+    description: String,
+    classroom: String,
+    coursesIds: Array,
+    hourStart: Number,
+    hourEnd: Number,
+    dateStart: Date,
+    durationDays: Number,
+    dateEnd: Date,
+    picture: {
+        thumbnail: String,
+        poster: {
+            url: String,
+            orientation: String
+        },
+        gallery: [String]
+    },
+    paid: Boolean,
+    paymentPrice: Number,
+    discussions: [
+        {
+            id: Number,
+            authorId: Number,
+            category: String,
+            title: String,
+            content: String,
+            upvotes: Number,
+            downvotes: Number,
+            usersVoted: [Number],
+            answers: [
+                {
+                    id: Number,
+                    authorId: Number,
+                    content: String,
+                    moment: String
+                }
+            ],
+            moment: String
+        }
+    ],
+    enrollments: [
+        {
+            userId: Number,
+            paid: Boolean,
+            moment: String
+        }
+    ]
+})
