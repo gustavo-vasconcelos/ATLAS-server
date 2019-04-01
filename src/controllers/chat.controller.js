90965d504bc44756a245d8e13df75c20adabae06
Gustavo <gustavofamoso48@gmail.com>
2019-04-01 02:06:00 +0100

    Added chat controller

---
+++ src/controllers/chat.controller.js
@@ -0,0 +1,12 @@
+module.exports = (http) => {
+    const io = require("socket.io")(http)
+    io.on("connection", function (socket) {
+        console.log("A user connected: " + socket.id)
+        socket.on("CHAT_MESSAGE", data => {
+            io.emit("CHAT_MESSAGE", data)
+        })
+    })
+
+}
+
+
