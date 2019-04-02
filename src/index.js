458dcf490bec919f9c5d31fd62fb9f12b9df958c
GUSTAVO HENRIQUE DA SILVA VASCONCELOS <9170196@ESEIG.PT>
2019-04-02 15:14:23 +0100

    Chat

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 
-const permittedLinker = ["http://localhost:8080", "http://atlas-vue.herokuapp.com/"]
+const permittedLinker = ["http://localhost:8080", "https://atlas-vue.herokuapp.com/"]
 
 app.use(function (req, res, next) {
     let i = 0, notFound = 1, referer = req.get("Referer")
