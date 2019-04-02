9b37a72cde8dd13f70ef235caa0d220a0c37f02a
Gustavo Vasconcelos <9170196@esmad.ipp.pt>
2019-04-02 23:35:19 +0000

    Requires all .route files automatically.

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -2,16 +2,8 @@ const fs = require("fs")
 const path = require("path")
 
 module.exports = app => {
-    /*
     fs
         .readdirSync(__dirname) // gets all filenames from /controllers
-        .filter(file => file.indexOf(".controller") !== -1) // filters filenames to only allow the ones containing ".controller"
-        .forEach(file => require(path.join(__dirname, file))(app)) // makes the require and passes app as a parameter
-    */
-   require("./users.route")(app)
-   require("./tags.route")(app)
-   require("./courses.route")(app)
-   require("./events.route")(app)
-   require("./chat.route")(app)
-   require("./auth.route")(app)
+        .filter(file => file.indexOf(".route") !== -1) // filters filenames to only allow the ones containing ".route"
+        .forEach(file => require(path.resolve(__dirname, file))(app)) // makes the require and passes app as a parameter
 }	
