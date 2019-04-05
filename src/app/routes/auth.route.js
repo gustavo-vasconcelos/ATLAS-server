655c4c6e0987022e7d3363d9aca5219e04b8acfa
CarlosGuedesPinto <carlosguedespinto@outlook.pt>
2019-04-05 04:17:10 -0700

    Assim sim

--- src/app/routes/auth.route.js
+++ src/app/routes/auth.route.js
@@ -8,3 +8,5 @@ router.post("/forgot-password", controller.forgotPassword)
 router.post("/reset-password", controller.resetPassword)
 
 module.exports = app => app.use("/auth", router)
+
+//E e assim que se programa. Mais nada
